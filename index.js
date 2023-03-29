12cf0b3457d2a6102dcf0d1d1c7746900800aa50
Samuel Burbano <me@iosamuel.dev>
2023-03-29 14:43:00 -0500

    feat: effects

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import { createSignal } from "solid";
+import { createSignal, createEffect } from "solid";
 
 const [count, setCount] = createSignal(0);
 
+createEffect(() => {
+  console.log("count changed", count());
+  document.querySelector("#count").textContent = count();
+});
+
 document.querySelector("button").addEventListener("click", () => {
   setCount(count() + 1);
-  document.querySelector("#count").textContent = count();
 });
--- solid.js
+++ solid.js
@@ -1,13 +1,24 @@
+let context = []; // Stack - LIFO (Last In First Out)
+
 export function createSignal(value) {
+  const subscriptions = new Set();
+
   const read = () => {
-    console.log("read");
+    const observer = context[context.length - 1];
+    if (observer) subscriptions.add(observer);
     return value;
   };
 
   const write = (newValue) => {
-    console.log("write");
     value = newValue;
+    subscriptions.forEach((observer) => observer());
   };
 
   return [read, write];
+}
+
+export function createEffect(fn) {
+  context.push(fn);
+  fn();
+  context.pop();
 }
