1cd32e6993623dc8eb9b35c16275d86c1ab69fe2
Samuel Burbano <me@iosamuel.dev>
2023-03-29 14:20:13 -0500

    feat: mini solid

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-import { createEffect, createMemo, createSignal } from "solid";
+import { solid } from "solid";
 
-const [getCount, setCount] = createSignal(0);
-const double = createMemo(() => getCount() * 2);
-
-createEffect(() => {
-  console.log("count changed", getCount());
-  document.querySelector("#count").textContent = getCount();
-});
-
-createEffect(() => {
-  console.log("double changed", double());
-  document.querySelector("#double").textContent = double();
-});
-
-document.querySelector("button").addEventListener("click", () => {
-  const number = Number(document.querySelector("#number").value);
-  setCount(getCount() + number);
-});
+solid();
