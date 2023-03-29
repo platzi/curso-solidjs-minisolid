0bb550370d6b3575d73d03cbd9372396ae1afddb
Samuel Burbano <me@iosamuel.dev>
2023-03-29 14:48:24 -0500

    feat: signals derivados

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import { createSignal, createEffect } from "solid";
 
 const [count, setCount] = createSignal(0);
+const doubleCount = () => count() * 2;
+const isDivisibleByThree = () => count() % 3 === 0;
 
 createEffect(() => {
   console.log("count changed", count());
   document.querySelector("#count").textContent = count();
+});
+
+createEffect(() => {
+  console.log("doubleCount changed", doubleCount());
+  document.querySelector("#double").textContent = doubleCount();
+});
+
+createEffect(() => {
+  console.log("isDivisibleByThree changed", isDivisibleByThree());
+  document.querySelector("#divisibleByThree").textContent =
+    isDivisibleByThree();
 });
 
 document.querySelector("button").addEventListener("click", () => {
