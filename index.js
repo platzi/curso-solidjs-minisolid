7fc9e3551a40681ab4d5c844fb7aea3ab6239a25
Samuel Burbano <me@iosamuel.dev>
2023-04-25 08:53:37 -0500

    feat: signals

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ const [count, setCount] = createSignal(0);
 
 document.querySelector("button").addEventListener("click", () => {
   setCount(count() + 1);
-  document.querySelector("#count").textContent = count();
 });
