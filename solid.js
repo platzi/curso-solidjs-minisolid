6ad18e034954b2c376813a31125249e817ca3a26
Samuel Burbano <me@iosamuel.dev>
2023-04-26 08:34:47 -0500

    feat: stores

--- solid.js
+++ solid.js
@@ -18,6 +18,37 @@ export function createSignal(value) {
   return [read, write];
 }
 
+export function createStore(value) {
+  const subscriptions = new Set();
+
+  /*
+    const [todos, setTodos] = createStore([
+      { text: "Learn Solid", done: false },
+    ])
+
+    createEffect(() => {
+      console.log("todos", todos[0].done)
+    })
+
+    setTodos('0', 'done', true)
+  */
+  const read = new Proxy(value, {
+    get(target, key) {
+      const observer = context[context.length - 1];
+      if (observer) subscriptions.add(observer);
+      return target[key];
+    },
+  });
+
+  const write = (...props) => {
+    const [index, key, newValue] = props;
+    value[index][key] = newValue;
+    subscriptions.forEach((observer) => observer());
+  };
+
+  return [read, write];
+}
+
 export function createEffect(fn) {
   context.push(fn);
   fn();
